acc7d0474c126a0cb3df371982fe40d2eea29474
asdjosegarcia <asdjosegarcia@gmail.com>
2023-04-02 12:51:09 -0300

    clase 14: guardado antes de reemplazar lo del reto mio por lo del profesor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import  Header  from '../components/Header';
+import React from "react";
+import Header from "../components/Header";
+import Main from "../components/Main";
 
 const home = () => {
-    return (
-        <Header/>
-    );
-}
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
 
 export default home;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom"; //
+import EmailSend from "../containers/EmailSend";
 import Layout from "../containers/Layout";
 import Login from "../containers/Login";
+import CreateAccount from "../containers/CreateAccount";
 import RecoveryPassword from "../containers/RecoveryPassword";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import "../styles/global.css";
+import EditAccount from "../containers/EditAccount";
+import Main from "../components/Main";
 
 const App = () => {
   return (
@@ -15,6 +19,9 @@ const App = () => {
           <Route exact path="/" component={Home} /> 
           {/* exact ya no es necesario con router 6.0 */}
           <Route exact path="/login" component={Login} />
+          <Route exact path="/EmailSend" component={EmailSend} /> 
+          <Route exact path="/CreateAccount" component={CreateAccount} /> 
+          <Route exact path="/EditAccount" component={EditAccount} /> 
           {/* path es la ruta */}
           <Route exact path="/recovery-password" component={RecoveryPassword} />
           <Route path={"*"} component={NotFound} /> {/* cualquier ruta que no sea las anteriores */}
