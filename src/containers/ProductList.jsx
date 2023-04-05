27280526d6db8c82ac05679a5efd4987944a0cfc
asdjosegarcia <asdjosegarcia@gmail.com>
2023-04-05 00:30:35 -0300

    clase 19: useEffect y consumo de api -add axios -add @babel/plugin-transform-runtinme

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';//useEffect nos permite hacer llamados a nuestra API y transmitirlo a nuestro componente
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
+import axios from 'axios'; //importamos a axios
+
+
+const API= 'https://api.escuelajs.co/api/v1/products'
 
 const ProductList = () => {
+	const [products,setProducts]=useState([])
+	useEffect(async()=>{
+		const response=await axios(API)//pedimos los datos a la api
+		setProducts(response.data)
+	},[])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product=>(
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
