a89cb6d61c953e5199e4a9aedfa880e831d6dc44
asdjosegarcia <asdjosegarcia@gmail.com>
2023-04-07 03:09:56 -0300

    clase 24: orden de compra  -add MyOrder menu toggle and functionality

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React,{useContext} from 'react';//llamamos a rect context
 import OrderItem from '../components/OrderItem';
+import AppContext from '../context/AppContext'; //llamamos a nuestro context
 import '../styles/MyOrder.scss';
 
+
+
 const MyOrder = () => {
+	const {state}=useContext(AppContext);//traemos el estado de nuestro contexto
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -10,7 +14,9 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map(product=>(
+					<OrderItem product={product} key={`orderItem-${product.id}`}/>//se le asigna un valor distinto a product id para que no se mezcle
+				))}
 				<div className="order">
 					<p>
 						<span>Total</span>
