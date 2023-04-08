ee650eef2a24948d33655026f244023e5933863e
asdjosegarcia <asdjosegarcia@gmail.com>
2023-04-08 05:37:04 -0300

    clase 25:calculo precio total  -add total price  functionality on myOrder.jsx

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -2,21 +2,24 @@ import React,{useContext} from 'react';//llamamos a rect context
 import OrderItem from '../components/OrderItem';
 import AppContext from '../context/AppContext'; //llamamos a nuestro context
 import '../styles/MyOrder.scss';
-
+import arrow from '@icons/flechita.svg'
 
 
 const MyOrder = () => {
 	const {state}=useContext(AppContext);//traemos el estado de nuestro contexto
-	/* const sumTotal=()=>{
-		const reducer=(accumulator,currentValue)=> accumulator+currentValue.price;
+	const sumTotal=()=>{
+		const reducer=(accumulator,currentValue)=> accumulator+currentValue.price;//acumulator suma total, current value valor actual
 		const sum=state.cart.reduce(reducer,0)
+		//state.cart entrega los valores
+		//.reduce() recive como parametro "reducer"(arrow function que suma 2 numeros) y 0 por que es el numero del elemento y objeto dentro del cada array
+		//se recive una funcion que entrega el valor de la suma, y se vuelve a sumar con el elemento 0 del array, y asi...
 		return sum;
-	} */
+	}
 
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
-				<img src="./icons/flechita.svg" alt="arrow" />
+				<img src={arrow} alt="arrow" />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
@@ -27,7 +30,7 @@ const MyOrder = () => {
 					<p>
 						<span>Total</span>
 					</p>
-					<p>{/* ${sumTotal()} */}</p>
+					<p> ${sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
