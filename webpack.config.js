5bd959d9d5a562799a35a020dbe6a946fec26c47
asdjosegarcia <asdjosegarcia@gmail.com>
2023-04-22 19:51:40 -0300

    deploy 1.16

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
-        publicPath:'/' /* para evitar problemas con las rutas? */
+        publicPath:'./' /* para evitar problemas con las rutas? */
     },
     mode: 'development',
 
