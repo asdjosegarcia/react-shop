48f772ea4fae2f84e2f0e9f026106f715d6e71b7
asdjosegarcia <asdjosegarcia@gmail.com>
2023-04-22 20:10:03 -0300

    deploy 1.17

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -22,8 +22,8 @@ const App = () => {
       <HashRouter>
         <Layout>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
+            <Route exact path="#/" component={Home} />
+            <Route exact path="#/login" component={Login} />
             <Route
               exact
               path="#/password-recovery"
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin= require('mini-css-extract-plugin')
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath:'./' /* para evitar problemas con las rutas? */
     },
