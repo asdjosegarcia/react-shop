a303660c62f762ea06ea886bef4302d95f83a376
asdjosegarcia <asdjosegarcia@gmail.com>
2023-04-22 05:30:12 -0300

    reto 6: mobile menu  -add toggle mobile menu  -fix myOrder mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react"; //traemos el use state
 import "@styles/Header.scss"; //@styles, gracias a que webpack le dice donde esta la carpeta styles
 import Menu from "@components/Menu";
+import MobileMenu from "@components/MobileMenu";
 import MyOrder from "../containers/MyOrder"; //importamos el componente my order
 import menuIcon from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
@@ -10,14 +11,23 @@ import shoppingCart from "@icons/icon_shopping_cart.svg";
 const Header = () => {
   const [toggle, setToggle] = useState(false); //togle de menu de cuentas (email)
   const [toggleOrders, setToggleOrders] = useState(false); //togle del carrito de compras (myorder)
+  const [mobileMenuToggle, setMobileMenuToggle] = useState(false);
   const { state } = useContext(AppContext); //importamos el estado "state" de nuestro custom hook mediante context
   const handleToggle = () => {
     setToggleOrders(false);
     setToggle(!toggle); //si es true pasa a false
   };
+  const handleMobileMenuToggle = () => {
+    setMobileMenuToggle(!mobileMenuToggle);
+  };
   return (
     <nav>
-      <img src={menuIcon} alt="menu" className="menu" />
+      <img
+        src={menuIcon}
+        alt="menu"
+        onClick={handleMobileMenuToggle}
+        className="menu"
+      />
       <div className="navbar-left">
         <img src={logo} alt="logo" className="nav-logo" />
         <ul>
@@ -48,7 +58,9 @@ const Header = () => {
           </li>
           <li //li del carrito
             className="navbar-shopping-cart"
-            onClick={() => {setToggleOrders(!toggleOrders), setToggle(false)}} //al hacer click establece un valor contrario a el actual
+            onClick={() => {
+              setToggleOrders(!toggleOrders), setToggle(false);
+            }} //al hacer click establece un valor contrario a el actual
           >
             <img src={shoppingCart} alt="shopping cart" />
             {state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
@@ -58,10 +70,13 @@ const Header = () => {
       </div>
       {toggle && <Menu />}
       {/* si toglle es true muestra la linea de codigo(el componente) */}
-      {toggleOrders && <MyOrder 
-      toggleOrders={toggleOrders}
-      setToggleOrders={setToggleOrders}
-      />}
+      {toggleOrders && (
+        <MyOrder
+        toggleOrders={toggleOrders}
+        setToggleOrders={setToggleOrders}
+        />
+        )}
+        {mobileMenuToggle && <MobileMenu />}
     </nav>
   );
 };
